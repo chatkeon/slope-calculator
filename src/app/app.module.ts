401fe36830b35f694203f176998f2a5ec4b38acb
chatkeon <chatkeon@gmail.com>
2023-07-02 14:33:45 -0700

    Added package-lock.json and placeholder code for variable component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,10 +14,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { AppComponent } from './app.component';
+import { VariableComponent } from './variable/variable.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VariableComponent
   ],
   imports: [
     BrowserModule,
